32672afb4dc6ad2e2e5b95599461a0e781e8050c
Flo2410 <florian@hye.dev>
2024-05-25 16:13:09 +0200

    Package git-fuzzy

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,8 @@ pkgs: with pkgs; {
   udev-stm32-named-tty = callPackage ./udev/stm32-named-tty.nix { };
   udev-saleae-logic = callPackage ./udev/saleae-logic.nix { };
 
+  git-fuzzy = callPackage ./git-fuzzy.nix { };
+
   # # Custom Packages
   # (callPackage ../../user/pkgs/home-assistant-desktop.nix { })
   # (callPackage ../../user/pkgs/elamx2.nix { })
---
+++ pkgs/git-fuzzy.nix
@@ -0,0 +1,23 @@
+{ stdenv, lib, fetchFromGitHub }:
+
+let
+  name = "git-fuzzy";
+
+in
+stdenv.mkDerivation {
+  pname = name;
+  version = "41b7691";
+
+  src = fetchFromGitHub {
+    owner = "bigH";
+    repo = name;
+    rev = "41b7691a837e23e36cec44e8ea2c071161727dfa";
+    sha256 = "sha256-fexv5aesUakrgaz4HE9Nt954OoBEF06qZb6VSMvuZhw=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+    cp -r . $out/
+    runHook postInstall
+  '';
+}
