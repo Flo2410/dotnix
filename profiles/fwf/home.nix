5c7fffd598c8ade50891c6db45da796046ea73b4
Flo2410 <florian@hye.dev>
2024-05-25 16:10:58 +0200

    Add nix-matlab to home.nix

--- profiles/fwf/home.nix
+++ profiles/fwf/home.nix
@@ -19,6 +19,8 @@
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
+
+      inputs.nix-matlab.overlay
     ];
 
     config.allowUnfree = true;
