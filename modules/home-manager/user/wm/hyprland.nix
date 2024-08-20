65e2573e289ccde83de50026692e381999de07d2
Flo2410 <florian@hye.dev>
2024-08-20 09:38:51 +0200

    Hypr: some more config

--- modules/home-manager/user/wm/hyprland.nix
+++ modules/home-manager/user/wm/hyprland.nix
@@ -21,7 +21,7 @@ in
       networkmanagerapplet # GUI for networkmanager
       waybar
       rofi-wayland
-
+      hyprlock
     ];
 
     wayland.windowManager.hyprland = {
@@ -42,19 +42,17 @@ in
         # ];
 
         "exec-once" = "${pkgs.writeShellScript "init-hyprland" ''
-          # initialize wallpaper daemon
-          #swww-daemon &
-          # set wallpaper
-          #swww img ${../../../../wallpapers/framework/Abstract_1-hue_logo.jpg} &
+          kwalletd6 &
 
           # networking
           nm-applet --indicator &
 
           waybar &
           dunst & # notifications
+          
         ''}";
 
-        monitor = "eDP-1, preferred, 0x0, 1.25";
+        monitor = "eDP-1, preferred, 0x0, 1.175";
 
         input = {
           kb_layout = "at";
@@ -66,13 +64,94 @@ in
           };
         };
 
+        gestures = {
+          workspace_swipe = true;
+        };
+
+        general = {
+          layout = "master";
+        };
+
+
+
 
         "$mod" = "SUPER";
         bind =
+          let
+            # Sources:
+            # https://github.com/fufexan/dotfiles/blob/a0bebadeb029ef50f2fa99ad3e7751a98b835610/home/programs/wayland/hyprland/binds.nix
+
+            # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+            workspaces = builtins.concatLists (builtins.genList
+              (
+                x:
+                let
+                  ws =
+                    let
+                      c = (x + 1) / 10;
+                    in
+                    builtins.toString (x + 1 - (c * 10));
+                in
+                [
+                  "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                  "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+                ]
+              )
+              10);
+
+            toggle = program: service:
+              let
+                prog = builtins.substring 0 14 program;
+                runserv = lib.optionalString service "run-as-service";
+              in
+              "pkill ${prog} || ${runserv} ${program}";
+
+            runOnce = program: "pgrep ${program} || ${program}";
+          in
           [
-            "$mod, SPACE, exec, rofi -show drun"
+            # --------------------------------
+            # compositor commands
+            # --------------------------------
+
+            "$mod SHIFT, E, exec, pkill Hyprland"
+            "$mod, Q, killactive,"
+            "$mod, F, fullscreen,"
+            "$mod, G, togglegroup,"
+            "$mod SHIFT, N, changegroupactive, f"
+            "$mod SHIFT, P, changegroupactive, b"
+            "$mod, R, togglesplit,"
+            "$mod, T, togglefloating,"
+            "$mod, P, pseudo,"
+            "$mod ALT, ,resizeactive,"
+
+            # --------------------------------
+            # move focus
+            # --------------------------------
+
+            "$mod, left, movefocus, l"
+            "$mod, right, movefocus, r"
+            "$mod, up, movefocus, u"
+            "$mod, down, movefocus, d"
+
+            # --------------------------------
+            # utility
+            # --------------------------------
+
+            # logout menu
+            "$mod, Escape, exec, ${toggle "wlogout" true} -p layer-shell"
+            # lock screen
+            "$mod, L, exec, ${runOnce "hyprlock"}"
+            # open settings
+            "$mod, U, exec, XDG_CURRENT_DESKTOP=gnome gnome-control-center"
+            # open app launcher
+            "ALT, SPACE, exec, pkill rofi || rofi -show drun"
+
+            # --------------------------------
+            # programs
+            # --------------------------------
+
             "$mod, RETURN, exec, kitty"
-          ];
+          ] ++ workspaces;
       };
     };
 
@@ -83,6 +162,26 @@ in
           preload = [ "${../../../../wallpapers/framework/Abstract_1-hue_logo.jpg}" ];
           wallpaper = [ ",${../../../../wallpapers/framework/Abstract_1-hue_logo.jpg}" ];
         };
+      };
+    };
+
+    programs.hyprlock = {
+      enable = true;
+
+      settings = {
+        general = {
+          disable_loading_bar = true;
+          hide_cursor = false;
+          no_fade_in = true;
+        };
+
+        background = [
+          {
+            monitor = "";
+            path = "${../../../../wallpapers/framework/Abstract_1-hue_logo.jpg}";
+          }
+        ];
+
       };
     };
 
