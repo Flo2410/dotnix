3fefcbbd348f7749d917ab097a5e87e50d2df426
Flo2410 <florian@hye.dev>
2024-08-23 22:18:22 +0200

    Hypridle: Fix brightness control

--- modules/home-manager/user/app/hypr/hypridle.nix
+++ modules/home-manager/user/app/hypr/hypridle.nix
@@ -23,14 +23,14 @@ in
         listener = [
           {
             timeout = 150; # 2.5 min
-            on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
-            on-resume = "brightnessctl -r"; # monitor backlight restore.
+            on-timeout = "brillo -l -O && brillo -l -S 0"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
+            on-resume = "brillo -l -I"; # monitor backlight restore.
           }
           # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
           {
             timeout = 150; # 2.5 min
-            on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
-            on-resume = " brightnessctl - rd rgb:kbd_backlight"; # turn on keyboard backlight.
+            on-timeout = "brillo -k -c -S 0 && brillo -k -S 0"; # turn off keyboard backlight.
+            on-resume = "brillo -k -S 50"; # turn on keyboard backlight.
           }
           {
             timeout = 300; # 5 min
