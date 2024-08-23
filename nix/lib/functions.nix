a92626c304b6a9e33b9e0e295cf4605ea4c863d2
Flo2410 <florian@hye.dev>
2024-08-23 18:50:15 +0200

    Add some lib functions

--- nix/lib/functions.nix
+++ nix/lib/functions.nix
@@ -5,5 +5,12 @@ with lib;
   lib.meta = {
     mkMutableSymlink = path: config.lib.file.mkOutOfStoreSymlink
       (config.user.home.dotfilesDirectory + removePrefix (toString inputs.self) (toString path));
+
+    mkIfElse = p: yes: no: mkMerge [
+      (mkIf p yes)
+      (mkIf (!p) no)
+    ];
+
+    runOnce = program: "pgrep ${program} || ${program}";
   };
 }
