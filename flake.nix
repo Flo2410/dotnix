e42858ee57cd86780ca74e0480809a4a817268e7
Flo2410 <florian@hye.dev>
2024-09-26 12:09:16 +0200

    Update and lock ags ref

--- flake.nix
+++ flake.nix
@@ -177,7 +177,7 @@
     catppuccin.url = "github:catppuccin/nix";
 
     ags = {
-      url = "github:Aylur/ags";
+      url = "github:Aylur/ags/fa6b26821d87133018e948c53dcd7387072008f5"; # Fix ags ref as top level gnome-bluetooth is not available in 24.05
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
