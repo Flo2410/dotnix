7666ba791b39eb523888e26e76d666fc7cdd291a
Flo2410 <florian@hye.dev>
2024-10-10 09:52:48 +0200

    VS Code: add cli arg to use gnome-keyring as keystore

--- modules/home-manager/user/app/vscode/vscode.nix
+++ modules/home-manager/user/app/vscode/vscode.nix
@@ -12,7 +12,9 @@ in
   config = mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      package = pkgs.unstable.vscode;
+      package = (pkgs.unstable.vscode.override {
+        commandLineArgs = "--password-store=\"gnome-libsecret\"";
+      });
       globalSnippets = importJSON ./vs-snippets.code-snippets;
       extensions = (import ./extensions.nix { inherit pkgs; });
       userSettings = (import ./settings.nix { inherit pkgs config; });
