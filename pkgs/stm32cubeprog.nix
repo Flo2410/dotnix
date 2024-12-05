70489c38a8ec02c7b4610905b498f9a7fca2a274
Flo2410 <florian@hye.dev>
2024-12-05 18:53:40 +0100

    pkgs: stm32cubeprog progress

--- pkgs/stm32cubeprog.nix
+++ pkgs/stm32cubeprog.nix
@@ -8,6 +8,8 @@
   name = "STM32CubeProg";
   pname = "stm32cubeprog";
   version = "2.16.0";
+
+  jdk_fx = pkgs.jdk8;
 in
   stdenv.mkDerivation {
     inherit name pname;
@@ -19,7 +21,7 @@ in
         Please proceed with the following steps to download and add it to the Nix
         store yourself:
         1. get en.STM32CubeMX_${builtins.replaceStrings ["."] ["-"] version}.zip
-        5. add the result to the store: nix-prefetch-url file://\$PWD/${name}
+        2. add the result to the store: nix-prefetch-url file://\$PWD/${name}
       '';
       sha256 = "0iz5ql69blg47hqwwj4w7jjbxnb7pmiqkbls0qdji2q261hzcqml";
     };
@@ -31,31 +33,77 @@ in
 
     nativeBuildInputs = with pkgs; [
       unzip
-      jre
       p7zip
+      makeWrapper
+      autoPatchelfHook
+
+      jdk_fx
+
+      libusb
+      stdenv.cc.cc.lib
+      zlib
+      libkrb5
+      libGL
+
+      libsForQt5.qt5.qtbase.out
+      openssl
+      pcsclite.lib
+
+      xorg.libX11
+      xorg.libxcb
     ];
 
     unpackPhase = ''
+      runHook preUnpack
       unzip -j $src SetupSTM32CubeProgrammer-${version}.linux SetupSTM32CubeProgrammer-${version}.exe
+      runHook postUnpack
     '';
 
     patchPhase = ''
+      runHook prePatch
+
       cp $extra_java AnalyticsPanelsConsoleHelper.java
-      ${pkgs.jre}/bin/javac -cp "SetupSTM32CubeProgrammer-${version}.exe" -d . AnalyticsPanelsConsoleHelper.java
+      ${jdk_fx}/bin/javac -cp "SetupSTM32CubeProgrammer-${version}.exe" -d . AnalyticsPanelsConsoleHelper.java
       7z a SetupSTM32CubeProgrammer-${version}.exe com/st/CustomPanels/AnalyticsPanelsConsoleHelper.class
+
+      runHook postPatch
     '';
 
     buildPhase = ''
-      mkdir -p $out/{bin,opt/STM32CubeProg}
-      ln -s ${pkgs.jre} jre
+      runHook preBuild
+
+      ln -s ${jdk_fx} jre
+
       chmod u+x SetupSTM32CubeProgrammer-${version}.linux
 
       echo "INSTALL_PATH=./build" > install.options
-      ./SetupSTM32CubeProgrammer-${version}.linux -options-auto install.options
+      mkdir build
+      set +e
+      echo "\n" | ./SetupSTM32CubeProgrammer-${version}.linux -options-auto install.options
+      set -e
+
+      runHook postBuild
     '';
 
     installPhase = ''
-      cp ./build $out/opt/STM32CubeProg
+      runHook preInstall
+
+      mkdir -p $out/{bin,opt/STM32CubeProg}
+      cp -a ./build/. $out/opt/STM32CubeProg
+      rm -r $out/opt/STM32CubeProg/updater
+      rm $out/opt/STM32CubeProg/bin/libssl.so
+
+      chmod u+x $out/opt/STM32CubeProg/bin/STM32_Programmer_CLI
+      chmod u+x $out/opt/STM32CubeProg/bin/STM32CubeProgrammerLauncher
+
+      makeWrapper ${jdk_fx}/bin/java $out/bin/${pname} \
+        --add-flags "-Djdk.gtk.version=3 -jar $out/opt/STM32CubeProg/bin/STM32CubeProgrammerLauncher"
+
+      runHook postInstall
+    '';
+
+    preFixup = ''
+      addAutoPatchelfSearchPath $out/opt/STM32CubeProg/lib
     '';
 
     meta = with lib; {
