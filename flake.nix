68174e4b0c933256bf83bad097fb0c248862a3b9
Flo2410 <florian@hye.dev>
2025-01-07 15:21:16 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -148,7 +148,6 @@
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
 
     vscode-server = {
