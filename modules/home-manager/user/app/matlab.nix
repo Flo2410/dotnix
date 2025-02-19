4b291f8d458da6f31c938f3e364aa64a24abbfa2
Flo2410 <florian@hye.dev>
2025-02-19 09:06:25 +0100

    Add some comments to Matlab module

--- modules/home-manager/user/app/matlab.nix
+++ modules/home-manager/user/app/matlab.nix
@@ -11,6 +11,32 @@ in {
     enable = mkEnableOption "matlab";
   };
 
+  # MATLAB                                                Version 24.2        (R2024b)
+  # Antenna Toolbox                                       Version 24.2        (R2024b)
+  # Communications Toolbox                                Version 24.2        (R2024b)
+  # Computer Vision Toolbox                               Version 24.2        (R2024b)
+  # DSP System Toolbox                                    Version 24.2        (R2024b)
+  # Image Processing Toolbox                              Version 24.2        (R2024b)
+  # Satellite Communications Toolbox                      Version 24.2        (R2024b)
+  # Signal Processing Toolbox                             Version 24.2        (R2024b)
+  # Statistics and Machine Learning Toolbox               Version 24.2        (R2024b)
+  # Symbolic Math Toolbox                                 Version 24.2        (R2024b)
+
+  # FHTW Moderne Regelungskonzepte
+  # Simulink
+  # Control System Toolbox
+  # Signal Processing Toolbox
+  # Simscape
+  # Simscape Electrical
+  # Simulink Control Design
+  # Symbolic Math Toolbox
+
+  # https://gitlab.com/doronbehar/nix-matlab
+  # For Installation:
+  # nix run gitlab:doronbehar/nix-matlab#matlab-shell
+  # To run MATLAB:
+  # nix run gitlab:doronbehar/nix-matlab
+
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       matlab # from nix-matlab
