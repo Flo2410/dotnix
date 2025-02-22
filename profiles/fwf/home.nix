e479c6b13203712056405261ab643549342ae041
Flo2410 <florian@hye.dev>
2025-02-22 17:30:19 +0100

    fwf: add mkshell script

--- profiles/fwf/home.nix
+++ profiles/fwf/home.nix
@@ -7,7 +7,7 @@
   config,
   pkgs,
   ...
-}: {
+}: rec {
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
@@ -209,6 +209,54 @@
       comment = "Launch CATIA V5 using Bottles.";
       startupWMClass = "CATIA V5";
     })
+
+    # Shell scrips
+    (pkgs.writeShellScriptBin "mkshell" ''
+      set -e
+
+      # Check if name is provided
+      if [ -z "$1" ]; then
+        printf "Usage: mkshell <shell>\n"
+        printf "  Available shells: latex, rust\n"
+        exit 1
+      fi
+
+      # Check if shell.nix or .envrc already exist
+      if [ -f shell.nix ] || [ -f .envrc ]; then
+        printf "\e[31mshell.nix or .envrc already exist\n"
+        exit 1
+      fi
+
+      if [ "$1" = "latex" ]; then
+        cp ${user.home.dotfilesDirectory}/nix/shells/latex.nix shell.nix
+      elif [ "$1" = "rust" ]; then
+        cp ${user.home.dotfilesDirectory}/nix/shells/rust.nix shell.nix
+      else
+        printf "\e[31mNo shell available for: $1\n"
+        exit 1
+      fi
+
+      # Create .envrc
+      printf "use nix\n" > .envrc
+
+      # Check if .gitignore does not exist
+      if [ ! -f .gitignore ]; then
+        printf "Downloading .gitignore for $1\n"
+
+        if [ "$1" = "latex" ]; then
+          ${pkgs.curl}/bin/curl https://raw.githubusercontent.com/github/gitignore/refs/heads/main/TeX.gitignore --output .gitignore --silent
+        elif [ "$1" = "rust" ]; then
+          ${pkgs.curl}/bin/curl https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Rust.gitignore --output .gitignore --silent
+        else
+          printf "\e[31mNo .gitignore for: $1\n"
+          exit 1
+        fi
+
+        printf "\n# direnv\n.direnv/\n" >> .gitignore
+      else
+        printf "\e[33m.gitignore already exists\n"
+      fi
+    '')
   ];
 
   # specialisation = {
