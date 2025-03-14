f066186f7a90391c970fea2f375b8dffdff51919
Flo2410 <florian@hye.dev>
2025-03-14 18:39:37 +0100

    Add some more config to nvim

--- modules/home-manager/user/app/nvim/default.nix
+++ modules/home-manager/user/app/nvim/default.nix
@@ -11,13 +11,47 @@ in {
     enable = mkEnableOption "nvim";
   };
 
+  # Resource: https://github.com/pete3n/nixvim-flake/
+
   config = mkIf cfg.enable {
     programs.nixvim = {
       enable = mkDefault true;
+      clipboard.providers.wl-copy.enable = true;
 
       colorschemes.catppuccin = {
         enable = true;
         settings.background.dark = "mocha";
+      };
+
+      globals = {
+        mapleader = " ";
+      };
+
+      opts = {
+        number = true;
+        relativenumber = true;
+        wrap = false;
+        undofile = true;
+        # colorcolumn = "90";
+        shiftwidth = 2;
+        tabstop = 2;
+        swapfile = false; # Undotree
+        backup = false; # Undotree
+      };
+
+      plugins = {
+        gitsigns.enable = true;
+        oil.enable = true;
+        undotree.enable = true;
+        fugitive.enable = true;
+        nvim-tree.enable = true;
+        web-devicons.enable = true;
+        lualine = {
+          enable = true;
+          iconsEnabled = false;
+          globalstatus = true;
+          theme = "onedark";
+        };
       };
     };
   };
