e1c8f518aa3db4e3f8ba3fe6cadc77e1cc5339d8
Flo2410 <florian@hye.dev>
2025-04-12 17:54:26 +0200

    pkgs: add stlink v3 to stlink udev

--- pkgs/udev/stm32-named-tty.nix
+++ pkgs/udev/stm32-named-tty.nix
@@ -12,10 +12,13 @@ stdenv.mkDerivation rec {
 
     text = ''
       ACTION=="add", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", SUBSYSTEM=="tty", SYMLINK+="stm32usb"
-
+      # ST Link v2.1
       ACTION=="add", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374b", SUBSYSTEM=="tty", SYMLINK+="stm32stlink"
+      # ST Link v3
+      SUBSYSTEM="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374e", MODE="0666", SYMLINK+="stlink-v3_%n"
 
       ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE="0666", TAG+="uaccess"
+
     '';
   };
 
