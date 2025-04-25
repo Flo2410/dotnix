f041788529dbac49196faf321ceecce6d29c0ed2
Flo2410 <florian@hye.dev>
2025-04-25 10:41:40 +0200

    Logiops: Automatically start service via udev rules

--- modules/nixos/system/app/logiops/logiops.nix
+++ modules/nixos/system/app/logiops/logiops.nix
@@ -15,6 +15,32 @@ in {
     # Install logiops package
     environment.systemPackages = [pkgs.logiops];
 
+    services.udev = {
+      enable = mkDefault true;
+      packages = [
+        (pkgs.stdenvNoCC.mkDerivation {
+          name = "LogiOps BLuetooth udev rules";
+          version = "1.0";
+
+          src = pkgs.writeTextFile {
+            name = "99-logiops-bluetooth.rules";
+
+            text = ''
+              ACTION=="add", SUBSYSTEM=="input", ATTRS{uniq}=="dc:8d:4d:24:a7:47", RUN+="${pkgs.systemd}/bin/systemctl start logiops.service"
+              ACTION=="remove", SUBSYSTEM=="input", ATTRS{uniq}=="dc:8d:4d:24:a7:47", RUN+="${pkgs.systemd}/bin/systemctl stop logiops.service"
+            '';
+          };
+
+          # do not unpack the source
+          dontUnpack = true;
+
+          installPhase = ''
+            install -D $src $out/lib/udev/rules.d/99-logiops-bluetooth.rules
+          '';
+        })
+      ];
+    };
+
     # Create systemd service
     systemd.services.logiops = {
       description = "An unofficial userspace driver for HID++ Logitech devices";
