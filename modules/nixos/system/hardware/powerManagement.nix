cea239fa0c0f284e845fc83997e262a217060a95
Flo2410 <florian@hye.dev>
2025-06-29 20:12:54 +0200

    pwr-mgmt: add TODOs

--- modules/nixos/system/hardware/powerManagement.nix
+++ modules/nixos/system/hardware/powerManagement.nix
@@ -30,9 +30,10 @@ in {
       # https://linuxize.com/post/create-a-linux-swap-file/
       # https://www.worldofbs.com/nixos-framework/#setting-up-hibernate
       # sudo findmnt -no UUID -T /swap/swapfile
-      boot.resumeDevice = "/dev/disk/by-uuid/62f508d6-bd71-4c41-8e81-13d152f898aa";
+      boot.resumeDevice = "/dev/disk/by-uuid/62f508d6-bd71-4c41-8e81-13d152f898aa"; # TODO Get uuid from hardware-config
       # sudo btrfs inspect-internal map-swapfile -r /swap/swapfile
       boot.kernelParams = [
+        # TODO: get offset from option
         "resume_offset=533760" # get the offset with ``sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}'`` https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Acquire_swap_file_offset
       ];
 
