91dea292448ce9dfb65680a70fc61443cfe5eea9
Flo2410 <florian@hye.dev>
2025-07-12 13:35:45 +0200

    pwr mgmt: add options to set resume device and offset.

--- modules/nixos/system/hardware/powerManagement.nix
+++ modules/nixos/system/hardware/powerManagement.nix
@@ -13,6 +13,12 @@ in {
       type = types.bool;
       default = false;
     };
+    resumeDevice = mkOption {
+      type = types.str;
+    };
+    resumeOffset = mkOption {
+      type = types.number;
+    };
   };
 
   config = mkMerge [
@@ -25,16 +31,15 @@ in {
       services.power-profiles-daemon.enable = true;
     })
 
-    (mkIf (cfg.enable && cfg.enableSuspendThenHibernate) {
+    (mkIf cfg.enableSuspendThenHibernate {
       # hibernate
       # https://linuxize.com/post/create-a-linux-swap-file/
       # https://www.worldofbs.com/nixos-framework/#setting-up-hibernate
       # sudo findmnt -no UUID -T /swap/swapfile
-      boot.resumeDevice = "/dev/disk/by-uuid/62f508d6-bd71-4c41-8e81-13d152f898aa"; # TODO Get uuid from hardware-config
+      boot.resumeDevice = cfg.resumeDevice;
       # sudo btrfs inspect-internal map-swapfile -r /swap/swapfile
       boot.kernelParams = [
-        # TODO: get offset from option
-        "resume_offset=533760" # get the offset with ``sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}'`` https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Acquire_swap_file_offset
+        "resume_offset=${toString cfg.resumeOffset}" # get the offset with ``sudo filefrag -v /swap/swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}'`` https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Acquire_swap_file_offset
       ];
 
       # Suspend-then-hibernate everywhere
