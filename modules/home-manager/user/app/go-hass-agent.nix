58d6a2e24a1cf354daf81dd9f85cbc0da1ca0422
Flo2410 <florian@hye.dev>
2025-07-29 20:54:01 +0200

    hm: add systemd service to go-hass-agent module

--- modules/home-manager/user/app/go-hass-agent.nix
+++ modules/home-manager/user/app/go-hass-agent.nix
@@ -38,6 +38,26 @@ in {
       go-hass-agent
     ];
 
+    systemd.user.services.go-hass-agent = {
+      # Source: https://github.com/joshuar/go-hass-agent/blob/5269b0c9fb1957c6efaf477c843c0b9e47e312b9/init/go-hass-agent.service
+      Unit = {
+        Description = "go-hass-agent";
+        Wants = ["network-online.target"];
+        After = ["network-online.target" "nss-lookup.target"];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.go-hass-agent}/bin/go-hass-agent --terminal run";
+        Type = "simple";
+        Restart = "always";
+        RestartSec = 30;
+      };
+
+      Install = {
+        WantedBy = ["multi-user.target"];
+      };
+    };
+
     xdg.configFile."go-hass-agent_commands" = mkIf (cfg.customCommands != {}) {
       target = "go-hass-agent/commands.toml";
       executable = false;
