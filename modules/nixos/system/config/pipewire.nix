d8a1b0e0e96405f4f0a1a2cbfc4c967f2f13123a
Flo2410 <florian@hye.dev>
2025-09-02 16:43:40 +0200

    pipewire: fix bluetooth not detecting the correct codecs

--- modules/nixos/system/config/pipewire.nix
+++ modules/nixos/system/config/pipewire.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 with lib; let
@@ -36,6 +37,17 @@ in {
             "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
           };
         };
+
+        configPackages = [
+          (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
+            monitor.bluez.properties = {
+              bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
+              bluez5.codecs = [ sbc sbc_xq aac ]
+              bluez5.enable-sbc-xq = true
+              bluez5.hfphsp-backend = "native"
+            }
+          '')
+        ];
       };
     };
   };
