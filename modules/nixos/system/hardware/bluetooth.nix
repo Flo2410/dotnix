0d22d3de1ca7d1f957af77d0d6d4ffd78993aa88
Flo2410 <florian@hye.dev>
2025-11-27 20:22:45 +0100

    bluetooth: added workaround for it not working

--- modules/nixos/system/hardware/bluetooth.nix
+++ modules/nixos/system/hardware/bluetooth.nix
@@ -15,6 +15,15 @@ in {
     hardware.bluetooth.enable = true;
     hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
+    # https://github.com/NixOS/nixpkgs/issues/170573
+    systemd.services."bluetooth".serviceConfig = {
+      StateDirectory = ""; # <<< minimal solution, applied in override.conf
+
+      # Seems unnecessary, but useful to keep in mind if bluetooth
+      # defaults get locked down even more:
+      # ReadWritePaths="/persist/var/lib/bluetooth/";
+    };
+
     # services.blueman.enable = true;
   };
 }
