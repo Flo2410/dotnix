9f8e9f36b07722c3ced398fea09641350d26ceac
Flo2410 <florian@hye.dev>
2025-12-07 11:52:28 +0100

    fwf: fix microphone

--- modules/nixos/system/config/pipewire.nix
+++ modules/nixos/system/config/pipewire.nix
@@ -29,12 +29,21 @@ in {
 
       wireplumber = {
         enable = true;
-        extraConfig.bluetoothEnhancements = {
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-            "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+
+        extraConfig = {
+          bluetoothEnhancements = {
+            "monitor.bluez.properties" = {
+              "bluez5.enable-sbc-xq" = true;
+              "bluez5.enable-msbc" = true;
+              "bluez5.enable-hw-volume" = true;
+              "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+            };
+          };
+
+          no-ucm = {
+            "monitor.alsa.properties" = {
+              "alsa.use-ucm" = false;
+            };
           };
         };
 
