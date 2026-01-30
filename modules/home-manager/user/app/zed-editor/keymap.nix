fcf931f992b9c3abb6365d64f3634ef15d9578f3
Flo2410 <florian@hye.dev>
2026-01-30 18:09:54 +0100

    zed: fix escape in vim mode.
    escape now closes the completions window without exiting insert mode.
    Also ctrl-space now toggles the completions window.

--- modules/home-manager/user/app/zed-editor/keymap.nix
+++ modules/home-manager/user/app/zed-editor/keymap.nix
@@ -12,12 +12,29 @@
     };
   }
   {
-    context = "vim_mode == insert";
+    context = "Editor && showing_completions";
     bindings = {
-      # "j k"= "vim::NormalBefore" // In insert mode, make jk escape to normal mode.
+      "ctrl-space" = "editor::Cancel";
     };
   }
-
+  {
+    context = "vim_mode == insert";
+    bindings = {
+      escape = null;
+    };
+  }
+  {
+    context = "vim_mode == insert && !showing_completions";
+    bindings = {
+      escape = "vim::NormalBefore";
+    };
+  }
+  {
+    context = "vim_mode == insert && showing_completions";
+    bindings = {
+      escape = "editor::Cancel";
+    };
+  }
   # Retore some keybinds in vim mode https=//zed.dev/docs/vim#restoring-common-text-editing-and-zed-keybindings
   {
     context = "Editor && !menu";
@@ -25,7 +42,7 @@
       "ctrl-c" = "editor::Copy"; # vim default: return to normal mode
       "ctrl-x" = "editor::Cut"; # vim default: decrement
       "ctrl-v" = "editor::Paste"; # vim default: visual block mode
-      "ctrl-y" = "editor::Undo"; # vim default: line up
+      "ctrl-y" = "editor::Redo"; # vim default: line up
       "ctrl-f" = "buffer_search::Deploy"; # vim default: page down
       "ctrl-o" = "workspace::Open"; # vim default: go back
       "ctrl-s" = "workspace::Save"; # vim default: show signature
@@ -33,7 +50,6 @@
       "ctrl-b" = "workspace::ToggleLeftDock"; # vim default: down
     };
   }
-
   {
     context = "(ProjectPanel && not_editing)";
     bindings = {
