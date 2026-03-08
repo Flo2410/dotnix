f5fcdc9fec17d75851077427befd3f60e554693f
Flo2410 <florian@hye.dev>
2026-03-08 18:18:37 +0100

    fs: added automount for paperless_consumption

--- modules/nixos/system/hardware/filesystem.nix
+++ modules/nixos/system/hardware/filesystem.nix
@@ -12,6 +12,7 @@ in {
     autoMounts = mkOption {
       type = types.listOf (types.enum [
         "/mnt/florian"
+        "/mnt/paperless_consumption"
       ]);
       default = [];
     };
@@ -22,24 +23,38 @@ in {
     boot.supportedFilesystems = ["cifs"];
 
     # //10.56.20.11/florian /mnt/florian cifs defaults,vers=3.0,rw,credentials=/home/florian/.smb-creds,iocharset=utf8,uid=1000,gid=1000,noauto,x-systemd.automount,x-systemd.idle-timeout=30,x-systemd.mount-timeout=10 0 0
-    fileSystems."/mnt/florian" = mkIf (builtins.elem "/mnt/florian" cfg.autoMounts) {
-      device = "//10.56.20.11/florian";
-      fsType = "cifs";
-      options = [
-        "defaults"
-        "vers=3.0"
-        "rw"
-        "credentials=/home/florian/.smb-creds"
-        "iocharset=utf8"
-        "uid=1000"
-        "gid=100"
-        "noauto"
-        "x-systemd.automount"
-        "x-systemd.idle-timeout=30"
-        "x-systemd.mount-timeout=5"
-        "soft"
-        "nofail"
-      ];
+    fileSystems = let
+      mkMount = {
+        local_path,
+        remote_path ? local_path,
+      }:
+        mkIf (builtins.elem "/mnt/${local_path}" cfg.autoMounts) {
+          device = "//10.56.20.11/${remote_path}";
+          fsType = "cifs";
+          options = [
+            "defaults"
+            "vers=3.0"
+            "rw"
+            "credentials=/home/florian/.smb-creds"
+            "iocharset=utf8"
+            "uid=1000"
+            "gid=100"
+            "noauto"
+            "x-systemd.automount"
+            "x-systemd.idle-timeout=30"
+            "x-systemd.mount-timeout=5"
+            "soft"
+            "nofail"
+          ];
+        };
+    in {
+      "/mnt/florian" = mkMount {
+        local_path = "florian";
+      };
+
+      "/mnt/paperless_consumption" = mkMount {
+        local_path = "paperless_consumption";
+      };
     };
   };
 }
