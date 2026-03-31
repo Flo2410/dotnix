462138c550271ac8e5f83e31d5eae9839970e150
Flo2410 <florian@hye.dev>
2026-03-31 15:17:30 +0200

    cachix: added hyprland cache

--- nix/cachix.nix
+++ nix/cachix.nix
@@ -5,11 +5,14 @@
         "https://nix-community.cachix.org"
         "https://cache.nixos-cuda.org"
         "https://ros.cachix.org"
+        "https://hyprland.cachix.org"
       ];
+      trusted-substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
