0bf2867970200110fd63ba03f96154e1e323b63a
Flo2410 <florian@hye.dev>
2026-04-01 10:32:39 +0200

    pkgs: update go-hass-agent

--- pkgs/go-hass-agent.nix
+++ pkgs/go-hass-agent.nix
@@ -2,61 +2,91 @@
 {
   lib,
   buildGoModule,
+  buildNpmPackage,
   fetchFromGitHub,
   pkg-config,
   xorg,
   mesa,
   glfw,
-}:
-buildGoModule (finalAttrs: {
+}: let
   pname = "go-hass-agent";
-  version = "13.3.3";
+  version = "14.10.3";
 
   src = fetchFromGitHub {
     owner = "joshuar";
     repo = "go-hass-agent";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-gvb/XuNAnypfgHqMnxFwn+8BXTJm4r1TvC8fwLmO5Bk=";
+    tag = "v${version}";
+    hash = "sha256-7F4zxxMKNrUiKonfO7dQQuODEnFgaFRM7Rzb7n1Erys=";
   };
 
-  vendorHash = "sha256-m+4kPNlfZBSW3TGsoYjburKdEo10ysV5AJCr5oDy9qU=";
+  jsDeps = buildNpmPackage {
+    inherit src version;
+    pname = "${pname}-js-deps";
+    npmDepsHash = "sha256-baO2S+NNgNgGjMNPrtmgaiiNTHv3vScOXQIVx1Xaxow=";
 
-  nativeBuildInputs = [
-    pkg-config
-  ];
+    buildPhase = ''
+      runHook preBuild
+      npm run build:js
+      npm run build:css
+      runHook postBuild
+    '';
 
-  subPackages = ["."];
-
-  buildInputs =
-    [
-      mesa
-      glfw
-    ]
-    ++ (with xorg; [
-      libX11
-      libXrandr
-      libXxf86vm
-      libXi
-      libXcursor
-      libXinerama
-      libXext
-      libxcb
-    ]);
-
-  postInstall = ''
-    mkdir -p $out/share/applications
-    mkdir -p $out/share/icons
-
-    cp $src/assets/go-hass-agent.desktop $out/share/applications/go-hass-agent.desktop
-    cp $src/internal/ui/assets/go-hass-agent.png $out/share/icons/go-hass-agent.png
-
-    substituteInPlace $out/share/applications/go-hass-agent.desktop \
-      --replace-warn "Exec=go-hass-agent" "Exec=$out/bin/go-hass-agent"
-  '';
-
-  meta = with lib; {
-    description = "Go-based Home Assistant agent";
-    homepage = "https://github.com/joshuar/go-hass-agent";
-    license = licenses.mit;
+    installPhase = ''
+      mkdir -p $out/lib/web/
+      cp ./web/content/*.{js,css} $out/lib/web
+    '';
   };
-})
+in
+  buildGoModule (finalAttrs: {
+    inherit src version pname;
+
+    vendorHash = "sha256-WPglpc8xqCW51LmdhGLAuB4jg96T72eRuaS61zagoNw=";
+
+    nativeBuildInputs = [
+      pkg-config
+    ];
+
+    subPackages = ["."];
+
+    buildInputs =
+      [
+        mesa
+        glfw
+      ]
+      ++ (with xorg; [
+        libX11
+        libXrandr
+        libXxf86vm
+        libXi
+        libXcursor
+        libXinerama
+        libXext
+        libxcb
+      ]);
+
+    ldflags = [
+      "-w -s -X github.com/joshuar/go-hass-agent/config.AppVersion=${version}"
+    ];
+
+    postPatch = ''
+      cp ${jsDeps}/lib/web/scripts.js ./web/content/scripts.js
+      cp ${jsDeps}/lib/web/styles.css ./web/content/styles.css
+    '';
+
+    # postInstall = ''
+    #   mkdir -p $out/share/applications
+    #   mkdir -p $out/share/icons
+
+    #   cp $src/assets/go-hass-agent.desktop $out/share/applications/go-hass-agent.desktop
+    #   cp $src/internal/ui/assets/go-hass-agent.png $out/share/icons/go-hass-agent.png
+
+    #   substituteInPlace $out/share/applications/go-hass-agent.desktop \
+    #     --replace-warn "Exec=go-hass-agent" "Exec=$out/bin/go-hass-agent"
+    # '';
+
+    meta = with lib; {
+      description = "Go-based Home Assistant agent";
+      homepage = "https://github.com/joshuar/go-hass-agent";
+      license = licenses.mit;
+    };
+  })
