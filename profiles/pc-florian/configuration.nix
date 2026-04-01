ad26825bc4f81ee91b15636e19c55465ea269809
Flo2410 <florian@hye.dev>
2026-04-01 14:07:22 +0200

    pc: remove override of greetd

--- profiles/pc-florian/configuration.nix
+++ profiles/pc-florian/configuration.nix
@@ -207,16 +207,6 @@
       # You should use the default configuration (which is no configuration), as that works the best out of the box.
       # However, if you need to configure something see https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md for configuration options and https://mynixos.com/nixpkgs/option/services.wivrn.config.json for an example configuration.
     };
-
-    greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "/share/wayland-sessions/hyprland-uwsm-fixed.desktop";
-          user = "florian";
-        };
-      };
-    };
   };
 
   security = {
@@ -224,6 +214,7 @@
       sshAgentAuth.enable = true;
       services = {
         sudo-rs.sshAgentAuth = true;
+        sddm.enableGnomeKeyring = true;
         login.enableGnomeKeyring = true;
       };
     };
